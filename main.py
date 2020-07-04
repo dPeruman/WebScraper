6b74e86354a1abd270fa878421c4b8ff8eaa5ca1
dheerajskylark <dheerajdeepu99@gmail.com>
2020-07-05 01:00:34 +0530

    Run this code to initiate the scraper

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
-from shine import shine_scraper
-from monster import monster_scraper
-from times_jobs import timesJobs_scraper
-from indeed import indeed_scraper
-from naukri2 import naukri_scraper
-from linkedin import linkedin_scraper
+import naukri2
+import monster
+import indeed
+import times_jobs
+import shine
+import linkedin
+
 
 """This takes info from user and returns scraped data from Naukri, monster, indeed, timejobs, shine and linkedin
 """
+
+## Download the chromedriver from : https://chromedriver.chromium.org/
+## And give the location of executable here
+
+PATH = './chromedriver.exe'
+
+naukri2.PATH = monster.PATH = indeed.PATH = times_jobs.PATH = shine.PATH = linkedin.PATH = PATH\
 
 def main():
     skill = input('Enter a skill')
     location = input('Enter location')
     experience = int(input('Enter experience in years'))
 
-    naukri_scraper(sk=skill, exp=experience, loc=location)
-    monster_scraper(sk=skill, exp=experience, loc=location)
-    indeed_scraper(sk=skill, exp=experience, loc=location)
-    timesJobs_scraper(sk=skill, exp=experience, loc=location)
-    shine_scraper(sk=skill, exp=experience, loc=location)
-    linkedin_scraper(sk=skill, loc=location)
+    naukri2.naukri_scraper(sk=skill, exp=experience, loc=location)
+    monster.monster_scraper(sk=skill, exp=experience, loc=location)
+    indeed.indeed_scraper(sk=skill, exp=experience, loc=location)
+    times_jobs.timesJobs_scraper(sk=skill, exp=experience, loc=location)
+    shine.shine_scraper(sk=skill, exp=experience, loc=location)
+    linkedin.linkedin_scraper(sk=skill, loc=location)
 
     
 
