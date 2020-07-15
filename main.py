fac13904330b843c47abb7528c58c6e88ee180c0
dheerajskylark <dheerajdeepu99@gmail.com>
2020-07-15 15:25:18 +0530

    Run this code to initiate the scraper

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import monster
 import indeed
 import times_jobs
 import shine
-import linkedin
+import linkedin 
+import pandas as pd
 
 
 """This takes info from user and returns scraped data from Naukri, monster, indeed, timejobs, shine and linkedin
@@ -12,23 +13,31 @@ import linkedin
 ## Download the chromedriver from : https://chromedriver.chromium.org/
 ## And give the location of executable here
 
-PATH = './chromedriver.exe'
+PATH = 'C:\\Users\\DHEERAJ SKYLARK\\Downloads\\chromedriver_win32\\chromedriver.exe'
 
-naukri2.PATH = monster.PATH = indeed.PATH = times_jobs.PATH = shine.PATH = linkedin.PATH = PATH\
+naukri2.PATH = monster.PATH = indeed.PATH = times_jobs.PATH = shine.PATH = linkedin.PATH = PATH
 
-def main():
+
+
+def main(dataframe):
     skill = input('Enter a skill: ')
     location = input('Enter location: ')
     experience = int(input('Enter experience in years: '))
 
-    naukri2.naukri_scraper(sk=skill, exp=experience, loc=location)
-    monster.monster_scraper(sk=skill, exp=experience, loc=location)
-    indeed.indeed_scraper(sk=skill, exp=experience, loc=location)
-    times_jobs.timesJobs_scraper(sk=skill, exp=experience, loc=location)
-    shine.shine_scraper(sk=skill, exp=experience, loc=location)
-    linkedin.linkedin_scraper(sk=skill, loc=location)
+    dataframe = naukri2.naukri_scraper(dataframe, sk=skill, exp=experience, loc=location)
+    dataframe = monster.monster_scraper(dataframe, sk=skill, exp=experience, loc=location)
+    #dataframe = indeed.indeed_scraper(dataframe, sk=skill, exp=experience, loc=location)
+    #dataframe = times_jobs.timesJobs_scraper(dataframe, sk=skill, exp=experience, loc=location)
+    #dataframe = shine.shine_scraper(dataframe, sk=skill, exp=experience, loc=location)
+    #dataframe = linkedin.linkedin_scraper(dataframe, sk=skill, loc=location)
 
-    
+    dataframe.to_csv("Jobs.csv",index=False)
 
 if __name__ == '__main__':
-    main()
+
+    dataframe = pd.DataFrame(columns = ['Recruiter name', 'Recruter tel', 'Recuiter mail id',
+                                        'Company website', 'Job locaion', 'Company name',
+                                        'Skill set required', 'Description url', 'Salary offered',
+                                        'Experience required', 'Qualification required'])
+
+    main(dataframe)
